357f304e605d6fd1ebae213a33e42f2ad77e0175
Harshgaddam <harshvardhangaddam@gmail.com>
2023-02-26 21:27:41 +0530

    Testing pre-commit hook

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,12 +1,5 @@
-/* eslint-disable curly */
-/* eslint-disable comma-dangle */
-/* eslint-disable no-var */
-/* eslint-disable prefer-const */
-/* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
-/* eslint-disable padded-blocks */
-/* eslint-disable semi */
-/* eslint-disable no-undef */
+let h = 1;
 const todoList = require("../todo");
 const { all, markAsComplete, add } = todoList();
 
