17ef7d8ec03c83db1711e681bab4695c575096bd
Harshgaddam <harshvardhangaddam@gmail.com>
2023-03-09 14:19:47 +0530

    Testing

--- todo-app/index.js
+++ todo-app/index.js
@@ -3,4 +3,6 @@ const app = express();
 app.get("/", function (request, response) {
   response.send("Hello World");
 });
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server Started");
+});
